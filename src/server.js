2105a48acbfa009115a4c8bd477f07a817481918
이성찬 <iseongchan@MacBook-Air.local>
2022-08-31 18:40:43 +0900

    #DAY3 wetube

--- src/server.js
+++ src/server.js
@@ -4,16 +4,32 @@ const PORT = "4000"
 
 const app = express();
 
-const handleHome = (req, res) => {
-    return res.send("<h1>I still love you.</h1>");
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
 };
-    
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Not Allowed<h1>")
+    }
+    console.log("Allowed, you may continue.");  
+    next();
+};
+
+const handleHome = (req, res) => {
+    return res.send("<h1>I love middlewares</h1>");
+};
+
 const handleLogin = (req, res) => {
     return res.send({ message: "Login here." });
 };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+// app.use() make global middleware
+app.use(privateMiddleware);
+
+app.get("/", logger, handleHome);
 
 const handleListening = () =>
     console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
