93850c5ea1d9fffade43f3b4547ba8a5336cff4a
이성찬 <iseongchan@MacBook-Air.local>
2022-09-01 14:32:21 +0900

    #DAY4 wetube

--- src/server.js
+++ src/server.js
@@ -1,35 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = "4000"
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
+const home = (req, res) => {
+    return res.send("hello");
 };
 
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected") {
-        return res.send("<h1>Not Allowed<h1>")
-    }
-    console.log("Allowed, you may continue.");  
-    next();
-};
-
-const handleHome = (req, res) => {
-    return res.send("<h1>I love middlewares</h1>");
-};
-
-const handleLogin = (req, res) => {
-    return res.send({ message: "Login here." });
+const login = (req, res) => {
+    return res.send("login");
 };
 
 // app.use() make global middleware
-app.use(privateMiddleware);
+app.use(logger);
 
-app.get("/", logger, handleHome);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
     console.log(`✅Server listening on port http://localhost:${PORT} 🚀`);
