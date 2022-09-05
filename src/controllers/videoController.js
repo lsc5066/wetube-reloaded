45800d4a47c7c60cfbd3be0a07946e3314ea3ade
이성찬 <iseongchan@MacBook-Air.local>
2022-09-06 01:48:20 +0900

    #DAY8 Wetube

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,36 @@
-export const trending = (req, res) => res.send("Homepage Videos");
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.send(`Watch video #${req.params.id}`);
-}
-export const edit = (req, res) => res.send("Edit");
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+    ];
+    res.render("home", { pageTitle: "Home", videos })
+};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search"); 
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-    console.log(req.params);
     return res.send("Delete Video");
 }
