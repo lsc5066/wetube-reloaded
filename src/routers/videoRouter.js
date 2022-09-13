441508a583543552ebbbd594c743f6567903444d
이성찬 <leesc332@gmail.com>
2022-09-13 23:52:29 +0900

    #6.25 Delete Video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
     postEdit,
     getUpload,
     postUpload,
+    deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,5 +13,6 @@ const videoRouter = express.Router();
 // /:@가 있어야 변수
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 export default videoRouter;
