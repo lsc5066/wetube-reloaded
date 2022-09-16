315ebfe87acc99352a3a80c96a402e81b0dfbded
이성찬 <leesc332@gmail.com>
2022-09-17 01:12:30 +0900

    #7.7 Sessions and Cookies part One

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,22 @@ app.set("views", process.cwd() + "/src/views");
 // app.use() make global middleware
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  })
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
