7574bd34af7e54548c63d08672feb93eada860cd
이성찬 <leesc332@gmail.com>
2022-09-22 18:10:44 +0900

    #8.14 Bugfix

--- src/models/Users.js
+++ src/models/Users.js
@@ -13,10 +13,11 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function () {
-  console.log("Users password: ", this.password);
-  this.password = await bcrypt.hash(this.password, 5);
+  if(this.isModified("password")){
+    this.password = await bcrypt.hash(this.password, 5);
+  }
   console.log("Hashed password", this.password);
-})
+});
 
 const User = mongoose.model("User", userSchema);
 export default User;
