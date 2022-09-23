8cdd639cdda8fcb1ea86223252777370209b509a
이성찬 <leesc332@gmail.com>
2022-09-24 00:29:34 +0900

    #11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,25 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handleplayClick = (e) => {
+  if(video.paused) {
+    playBtn.innerText = "Pause";
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePause = () => (playBtn.innerText = "Play");
+const handleplay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {
+
+};
+
+playBtn.addEventListener("click", handleplayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.aaddEventListener("click", handleplay);
