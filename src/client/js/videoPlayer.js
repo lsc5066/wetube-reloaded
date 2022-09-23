4a6d1c7a51dc4d5698f1a0357f27d4b9764209da
이성찬 <leesc332@gmail.com>
2022-09-24 00:38:49 +0900

    #11.2 Mute and Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,24 +2,26 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handleplayClick = (e) => {
-  if(video.paused) {
-    playBtn.innerText = "Pause";
+  if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-const handlePause = () => (playBtn.innerText = "Play");
-const handleplay = () => (playBtn.innerText = "Pause");
 
 const handleMute = (e) => {
-
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
 };
 
 playBtn.addEventListener("click", handleplayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.aaddEventListener("click", handleplay);
