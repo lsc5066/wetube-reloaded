a6f5973027b434cbae79e95cdce2a12e21f5ae0f
이성찬 <leesc332@gmail.com>
2022-10-06 17:13:41 +0900

    #16.6 Rendering Comments

--- src/models/User.js
+++ src/models/User.js
@@ -9,15 +9,14 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   name: { type: String, required: true },
   location: String,
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref:"Comment" }],
-  videos: [{type: mongoose.Schema.Types.ObjectId, required: true, ref:"Video" }],
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
 
 userSchema.pre("save", async function () {
-  if(this.isModified("password")){
+  if (this.isModified("password")) {
     this.password = await bcrypt.hash(this.password, 5);
   }
-  console.log("Hashed password", this.password);
 });
 
 const User = mongoose.model("User", userSchema);
