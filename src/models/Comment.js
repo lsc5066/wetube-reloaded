8a67d1fd1d12e107fd0af9830c550ecec8aa6b50
이성찬 <leesc332@gmail.com>
2022-10-06 16:28:14 +0900

    #16.5 Commenting

--- src/models/Comment.js
+++ src/models/Comment.js
@@ -7,7 +7,7 @@ const commentSchema = new mongoose.Schema({
   owner: {
     type: mongoose.Schema.Types.ObjectId, required: true, ref: "User"
   },
-  Video: {
+  video: {
     type: mongoose.Schema.Types.ObjectId, required: true, ref: "Video"
   },
   createdAt: {
