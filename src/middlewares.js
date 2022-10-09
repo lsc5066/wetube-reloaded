ce3033c8059624ae347b41b37554763b2e04ff0b
이성찬 <leesc332@gmail.com>
2022-10-09 23:02:54 +0900

    #17.9 Production Environment

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,16 +9,25 @@ const s3 = new aws.S3({
   }
 })
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
   s3: s3,
-  bucket: "wetube-leesc332",
+  bucket: "wetube-leesc332/images",
   acl: "public-read",
-})
+});
+
+const s3VideoUploader = multerS3({
+  s3: s3,
+  bucket: "wetube-leesc332/videos",
+  acl: "public-read",
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
+  res.locals.isHeroku = isHeroku;
   next();
 };
 
@@ -45,7 +54,7 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
-  storage: multerUploader
+  storage: isHeroku ? s3ImageUploader : undefined,
 });
 
 export const videoUpload = multer({
@@ -53,5 +62,5 @@ export const videoUpload = multer({
   limits: {
     fileSize: 10000000,
   },
-  storage: multerUploader
+  storage: isHeroku ? s3VideoUploader : undefined,
 });
