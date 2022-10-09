158def5c2f14066a310934d6802c78d5df62c725
이성찬 <leesc332@gmail.com>
2022-10-09 20:26:54 +0900

    #17.5 PORT env variable

--- src/init.js
+++ src/init.js
@@ -2,9 +2,10 @@ import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
+import "./models/Comment";
 import app from "./server";
 
-const PORT = "4000"
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
     console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
